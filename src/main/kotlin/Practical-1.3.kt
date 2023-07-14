a085aba3d0e25dbe11b312fff0ecf5154ba07c79
Harsh <harshsshah2001@gmail.com>
2023-07-14 21:19:07 +0530

    Commit

--- src/main/kotlin/Practical-1.3.kt
+++ src/main/kotlin/Practical-1.3.kt
@@ -1,4 +1,19 @@
 fun main(){
-    print("Enter a Your Name")
-    val a= readLine()
+    println("Enter a Your Name : ")
+    val aa= readLine()
+    println("Enter Your Enrollment Number : ")
+    val b= readLine()
+    println("Enter Your Collage Name : ")
+    val c= readLine()
+    println("Enter a Branch Name : ")
+    val d= readLine()
+    println("Enter a Student Age : ")
+    val e= readLine()
+  println("Student Data : ")
+  println("Student Name : $aa")
+    println("Student Enrolment No : $b")
+    println("Collage Name : $c")
+    println("Collage Name : $d")
+    println("Collage Name : $e")
+
 }
