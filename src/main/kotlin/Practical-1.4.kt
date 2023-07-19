29ab206c5208cbef5382587e2d3bcb9efc065d4a
student <student@gnu.ac.in>
2023-07-19 08:43:13 +0530

    Commit

---
+++ src/main/kotlin/Practical-1.4.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("Odd Even Program")
+    println("Enter a Number")
+    val a= readLine()!!.toInt()
+    if(a%2==0) {
+        println("$a is Even Number")
+    } else{
+        println("$a is Odd Number")
+}
+}
