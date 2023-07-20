e443e76167ef1d3d003913b45d937d5ec4255024
harshsshah2001 <139530635+harshsshah2001@users.noreply.github.com>
2023-07-20 18:42:40 +0530

    Add files via upload
    Commit

---
+++ src/main/kotlin/Practical-1.6.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+    val a = 10
+    val b = 20
+    addtion(a,b)
+    subtraction(a,b)
+    multiplication(a,b)
+    divison(a,b)
+}
+fun addtion(a:Int,b:Int){
+    var c = a+b;
+    println("The Addition Is $c")
+}
+fun subtraction(a:Int,b:Int){
+    var s = a-b;
+    println("The Subtraction Is $s")
+}
+fun multiplication(a:Int,b:Int){
+    var m = a+b;
+    println("The Multiplication Is $m")
+}
+fun divison(a:Int,b:Int){
+    var d = a//b;
+    println("The Divison Is $d")
+}
